8abfc167ac821ef2ce66f1619c33dee20214477d
Hiroyuki Kondo <hombredeestemundo@gmail.com>
2012-09-05 17:41:46 +0900

    fixed #93 wishlist nav-bar link modified

--- app/overrides/insert_top_main_nav_bar.rb
+++ app/overrides/insert_top_main_nav_bar.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :name => "insert_top_main_nav_bar",
                      :insert_top => "ul#nav-bar",
-                     :erb => "<li><%= link_to t('wishlists'), wishlists_path %></li>"
+                     :erb => "<% if user_signed_in? %><li><%= link_to t('wishlists'), wishlists_path %></li><% end %>"
                      )
